00e659c2195ca348fa1a12cf1582327824518b0a
ajvejko <moucka.matej01@gmail.com>
2023-03-05 01:03:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const canvas = ref<HTMLCanvasElement | null>(null);
+const ctx = ref<CanvasRenderingContext2D | null>(null);
+
+
+
+onMounted(() => {
+  ctx.value = canvas.value?.getContext("2d") || null;
+  render();
+});
+
+const render = () => {
+  if (!ctx.value) {
+    return;
+  }
+  ctx.value.fillStyle = "#10b981";
+  ctx.value.beginPath();
+  ctx.value.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.value.fill();
+  ctx.value.save();
+};
+</script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <canvas class="border-2" width="500" height="500" ref="canvas"></canvas>
+  </div>
+</template>
---
+++ src/Store/Entries.ts
@@ -0,0 +1,21 @@
+import { ref } from "vue";
+
+let numEntry = ref(0); //Amount of entries
+let entryNames: string[] = [
+  //List of entries
+  "Entry1",
+  "Entry2",
+  "Entry3",
+  "Entry4",
+  "Entry5",
+  "Entry6",
+];
+const entryColors: string[] = [
+  // List of colors that are used to generate the wheel
+  "#16A34A", //Green
+  "#2563EB", //Blue
+  "#7C3AED", //Purple
+  "#DB2777", //Pink
+  "#DC2626", //Red
+  "FACC15", //Yellow
+];
