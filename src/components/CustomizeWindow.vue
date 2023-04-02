9fe9630f8c30c003605766c2ff3121e5596fd7b6
ajvejko <moucka.matej01@gmail.com>
2023-04-02 22:34:12 +0200

    added Config Window template

---
+++ src/components/CustomizeWindow.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="absolute top-0 z-20 flex h-screen w-screen justify-center bg-black/50"
+  >
+    <div class="relative h-fit w-3/4 rounded-xl border-2 bg-neutral-900">
+      Hello World
+    </div>
+  </div>
+</template>
